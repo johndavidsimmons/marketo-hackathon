57d04dbde7e20a6aa449b7974cea382e0679cf18
John Simmons <johnsimmons@johnsimmonsmbp.lan>
2019-10-03 15:32:01 -0400

    added submit success

--- src/lib/events/formSubmitSuccess.js
+++ src/lib/events/formSubmitSuccess.js
@@ -2,4 +2,13 @@
 
 module.exports = function(settings, trigger) {
   // TODO Start watching for an event. Call trigger when the event occurs.
+    var MktoForms2 = window.MktoForms2;
+    if (MktoForms2) {
+    	MktoForms2.whenReady(function handleReady (form) { 
+    		form.onSuccess(function handleSuccess(values) {
+    			console.log("form submit success")
+    			trigger;
+    		})
+    	}
+    }
 };

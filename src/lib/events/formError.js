4695cec7a5c91762385cfd205f84225c628aa1ba
John Simmons <johnsimmons@johnsimmonsmbp.lan>
2019-10-03 12:35:10 -0400

    added formerror

--- src/lib/events/formError.js
+++ src/lib/events/formError.js
@@ -2,4 +2,34 @@
 
 module.exports = function(settings, trigger) {
   // TODO Start watching for an event. Call trigger when the event occurs.
+  if (MktoForms2) {
+    MktoForms2.whenReady(function handleReady(form) {
+
+      digitalData.page.pageInfo.mktoFormId = form.getId();
+      digitalData.page.pageInfo.mktoMunchkinId = form.getValues().munchkinId;
+      mrk_form_loaded = true;
+
+      form.onValidate(function handleValidate(validateForm) {
+        // wait for the error message to appear
+        setTimeout(function() {
+          var element, mktoErrorMsg;
+          window.formValid = form;
+
+          // check for error message
+          element = document.querySelector(".mktoErrorMsg");
+          if (element) {
+
+            var mktoErrorField = document.querySelector(".mktoError").previousSibling.getAttribute('name');
+            mktoErrorMsg = element.textContent || element.innerText;
+            mktoError = mktoErrorField + ":" + mktoErrorMsg
+
+            var mktoError = mktoErrorField + ":" + mktoErrorMsg
+            trigger();
+          }
+
+
+        }, 50); //timeout
+      }); //onvalidate
+    }); //whenready
+  } //if
 };

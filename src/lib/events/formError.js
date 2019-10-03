9f651126c5320479ccb24ee0c64181ce45a83f36
John Simmons <johnsimmons@johnsimmonsmbp.lan>
2019-10-03 17:46:25 -0400

    add global mkto to formerror

--- src/lib/events/formError.js
+++ src/lib/events/formError.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = function(settings, trigger) {
+  var MktoForms2 = window.MktoForms2;
   if (MktoForms2) {
     MktoForms2.whenReady(function handleReady(form) {
       form.onValidate(function handleValidate(validateForm) {
